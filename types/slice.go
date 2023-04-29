a610e81fb5b20c994fd8c78c2f0f2138f3b1e01f
Adel Urazov <adel.i.urazov@ya.ru>
2023-04-29 13:49:32 +0300

    Added Additional Methods For Slice

--- types/slice.go
+++ types/slice.go
@@ -22,3 +22,20 @@ func (s Slice[T]) Len() Size {
 func (s Slice[T]) Add(v ...T) Slice[T] {
 	return append(s, v...)
 }
+
+func (s Slice[T]) Get(i Size) T {
+	return s[i]
+}
+
+func (s Slice[T]) Set(i Size, v T) Slice[T] {
+	s[i] = v
+	return s
+}
+
+func (s Slice[T]) Copy() Slice[T] {
+	return append(Slice[T]{}, s...)
+}
+
+func (s Slice[T]) Remove(i Size) Slice[T] {
+	return append(s[:i], s[i+1:]...)
+}
